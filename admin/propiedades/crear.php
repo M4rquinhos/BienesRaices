598d0bb3b23e3d53f40d64411cb23888d04d3d63
Marco Carrillo <marco.37099p@gmail.com>
2023-04-03 20:45:58 -0600

    Validando el formulario

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,4 +1,71 @@
 <?php
+    //base de datos 
+    require '../../includes/config/database.php';
+    $db = conectarDB();
+
+    //arreglo con mensajes de errores 
+    $errores = [];
+
+    //Ejecuta el codigo despues de que el usuario envia el formulario
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // echo "<pre>";
+        // echo    var_dump($_POST);
+        // echo "</pre>";
+
+        $titulo = $_POST['titulo'];
+        $precio = $_POST['precio'];
+        $descripcion = $_POST['descripcion'];
+        $habitaciones = $_POST['habitaciones'];
+        $wc = $_POST['wc'];
+        $estacionamiento = $_POST['estacionamiento'];
+        $vendedor = $_POST['vendedor'];
+
+        if (!$titulo) {
+            $errores[] = "Debes añadir un titulo";
+        }
+
+        if (!$precio) {
+            $errores[] = "El precio es obligatorio";
+        }
+
+        if (strlen( $descripcion ) < 50) {
+            $errores[] = "La descripcion es obligatoria y debe tener al menos 50 caracteres";
+        }
+
+        if (!$habitaciones) {
+            $errores[] = "El numero de habitaciones es obligatorio";
+        }
+
+        if (!$wc) {
+            $errores[] = "El numero de baños es obligatorio";
+        }
+
+        if (!$estacionamiento) {
+            $errores[] = "El numero de estacionamientos es obligatorio";
+        }
+
+        if (!$vendedor) {
+            $errores[] = "Elige un vendedor";
+        }
+
+        //revisar que el arreglo este vacio para insertar
+        if (empty($errores)) {
+            //insertar en la base de datos
+            $query = " INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedores_id) 
+            VALUES ('$titulo', '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedor')";
+
+            // echo $query;
+
+            $resultado = mysqli_query($db, $query);
+
+            if ($resultado) {
+                echo "insertado correctamente";
+            }
+        }
+
+        
+    }
+
     require '../../includes/funciones.php';
     incluirTemplate('header');
  ?>
@@ -7,6 +74,55 @@
         <h1>Crear</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
+
+        <?php foreach ($errores as $error): ?>
+            <div class="alerta error">
+                <?=$error?>
+            </div>
+        <?php endforeach; ?>
+
+        <form class="formulario" method="POST" action="/admin/propiedades/crear.php">
+
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo:</label>
+                <input type="text" id="titulo" name="titulo" placeholder="Titulo propiedad">
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" name="precio" placeholder="Precio propiedad">
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" name="imagen" accept="image/jpeg/png">
+
+                <label for="descripcion">Descripción:</label>
+                <textarea id="descripcion" name="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información de la propiedad</legend>
+
+                <label for="habitaciones">Habitaciones:</label>
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Titulo propiedad" min="1" max="9">
+
+                <label for="wc">Baños:</label>
+                <input type="number" id="wc" name="wc" placeholder="Titulo propiedad" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento:</label>
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Titulo propiedad" min="1" max="9">
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select name="vendedor">
+                    <option value="1">Juan</option>
+                    <option value="2">Karen</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+        </form>
     </main>
 
 <?php 
--- includes/config/database.php
+++ includes/config/database.php
@@ -1,1 +1,12 @@
 <?php
+
+
+function conectarDB() : mysqli {
+    $db = mysqli_connect('localhost', 'root', '', 'bienesraices_crud', 3306);
+    if (!$db) {
+        echo "Error en la conexión";
+        exit;
+    }
+
+    return $db;
+}
