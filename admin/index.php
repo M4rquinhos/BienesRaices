e5c1f4beea084803c55a07d8d0d8d45a30f79087
Marco Carrillo <marco.37099p@gmail.com>
2023-04-06 03:10:57 -0600

    Listando propiedades

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,19 @@
 <?php
+
+    //imoprtar conexion a la basde de datos
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    //Escribir el query
+    $query = "SELECT * FROM propiedades";
+
+    //Consultar la basde de datos
+    $resultadoConsulta = mysqli_query($db, $query);
+
+    //muestra mensaje condicional (creacion de una nueva propiedad)
     $resultado = $_GET['resultado'] ?? null;
+
+    //incluye el template que se le indica
     require '../includes/funciones.php';
     incluirTemplate('header');
  ?>
@@ -12,8 +26,40 @@
         <?php endif; ?>
 
         <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+
+
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Titulo</th>
+                    <th>Imagen</th>
+                    <th>Precio</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody> <!-- Mostrar los resultados de la base de datos -->
+            <?php while( $row = mysqli_fetch_assoc($resultadoConsulta) ): ?>
+                <tr>
+                    <td><?=$row['id'];?></td>
+                    <td><?=$row['titulo'];?></td>
+                    <td> <img src="/imagenes/<?=$row['imagen'];?>"  class="imagen-tabla"> </td>
+                    <td>$ <?=$row['precio'];?></td>
+                    <td>
+                        <a href="#" class="boton-rojo-block">Eliminar</a>
+                        <a href="#" class="boton-amarillo-block">Actualizar</a>
+                    </td>
+                </tr>
+            <?php endwhile; ?>
+            </tbody>
+        </table>
     </main>
 
 <?php 
+
+    //cerrar la conexion a la base de datos
+    mysqli_close($db);
+
+
     incluirTemplate('footer');
 ?>   
